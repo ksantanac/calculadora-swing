a6dbb264042dfeae214c44b344ef703e3bf29a29
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-02-08 22:46:31 -0300

    Iniciando o layout do teclado.

--- src/br/com/cod3r/calc/visao/Teclado.java
+++ src/br/com/cod3r/calc/visao/Teclado.java
@@ -12,6 +12,48 @@ public class Teclado extends JPanel {
 
     public Teclado() {
 
+        GridBagLayout layout = new GridBagLayout();
+        GridBagConstraints c = new GridBagConstraints();
+
+        setLayout(layout);
+
+        // Linha 1
+        adicionarBotao("AC", CINZA_ESCURO, c, 0, 0);
+        adicionarBotao("+/-", CINZA_ESCURO, c, 1, 0);
+        adicionarBotao("%", CINZA_ESCURO, c, 2, 0);
+        adicionarBotao("/", LARANJA, c, 3, 0);
+
+        // Linha 2
+        adicionarBotao("7", CINZA_CLARO, c, 0, 1);
+        adicionarBotao("8", CINZA_CLARO, c, 1, 1);
+        adicionarBotao("9", CINZA_CLARO, c, 2, 1);
+        adicionarBotao("*", LARANJA, c, 3, 1);
+
+        // Linha 3
+        adicionarBotao("4", CINZA_CLARO, c, 0, 2);
+        adicionarBotao("5", CINZA_CLARO, c, 1, 2);
+        adicionarBotao("6", CINZA_CLARO, c, 2, 2);
+        adicionarBotao("-", LARANJA, c, 3, 2);
+
+        // Linha 4
+        adicionarBotao("1", CINZA_CLARO, c, 0, 3);
+        adicionarBotao("2", CINZA_CLARO, c, 1, 3);
+        adicionarBotao("3", CINZA_CLARO, c, 2, 3);
+        adicionarBotao("+", LARANJA, c, 3, 3);
+
+        // Linha 5
+        adicionarBotao("0", CINZA_CLARO, c, 0, 4);
+        adicionarBotao("0", CINZA_CLARO, c, 1, 4);
+        adicionarBotao(",", CINZA_CLARO, c, 2, 4);
+        adicionarBotao("=", LARANJA, c, 3, 4);
+
+    }
+
+    private void adicionarBotao(String texto, Color cor, GridBagConstraints c, int x, int y) {
+        c.gridx = x;
+        c.gridy = y;
+        Botao botao = new Botao(texto, cor);
+        add(botao);
     }
 
 }
