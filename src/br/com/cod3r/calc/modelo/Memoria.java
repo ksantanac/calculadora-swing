64b6f651615cbbc6b8afff9192e519980a936417
Kaue Matheus Santana <kauesantana_13@hotmail.com>
2025-02-09 13:38:13 -0300

    Lógica calculadora 02.

--- src/br/com/cod3r/calc/modelo/Memoria.java
+++ src/br/com/cod3r/calc/modelo/Memoria.java
@@ -1,8 +1,13 @@
 package br.com.cod3r.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
 
     private static final Memoria instancia = new Memoria();
+
+    private final List<MemoriaObservador> observadores = new ArrayList<>();
 
     private String textoAtual = "";
 
@@ -14,8 +19,19 @@ public class Memoria {
         return instancia;
     }
 
+    public void adicionarObservador(MemoriaObservador observador) {
+        observadores.add(observador);
+    }
+
     public String getTextoAtual() {
         return textoAtual.isEmpty() ? "0" : textoAtual;
+    }
+
+    public void processarComando(String valor) {
+
+
+        textoAtual += valor;
+        observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
     }
 
 }
--- src/br/com/cod3r/calc/visao/Display.java
+++ src/br/com/cod3r/calc/visao/Display.java
@@ -1,16 +1,18 @@
 package br.com.cod3r.calc.visao;
 
 import br.com.cod3r.calc.modelo.Memoria;
+import br.com.cod3r.calc.modelo.MemoriaObservador;
 
 import javax.swing.*;
 import java.awt.*;
 
 
-public class Display extends JPanel {
+public class Display extends JPanel implements MemoriaObservador {
 
     private final JLabel label;
 
     public Display() {
+        Memoria.getInstancia().adicionarObservador(this);
 
         setBackground(new Color(46, 49, 50));
 
@@ -21,5 +23,10 @@ public class Display extends JPanel {
         setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 24));
 
         add(label);
+    }
+
+    @Override
+    public void valorAlterado(String novoValor) {
+        label.setText(novoValor);
     }
 }
